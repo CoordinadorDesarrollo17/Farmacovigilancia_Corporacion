5841de4bb70a4d6e803a7f10af9f2b390d0759d5
COBEFAR\TI-3 <soporte02.desarrollo>
2026-03-16 16:56:41 -0500

    agregando gitignore y limpiando archivos temporales

--- Sln_Farmacovigilancia_v2/obj/Debug/net9.0/Sln_Farmacovigilancia_v2.AssemblyInfo.cs
+++ Sln_Farmacovigilancia_v2/obj/Debug/net9.0/Sln_Farmacovigilancia_v2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sln_Farmacovigilancia_v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db5a2383c61b74d63121041dd3686e0bf69671c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sln_Farmacovigilancia_v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sln_Farmacovigilancia_v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
